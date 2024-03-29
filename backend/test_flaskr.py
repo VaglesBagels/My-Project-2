eacf21a3ac582cb847840f0e47df904195e519ef
VaglesBagels <104954408+VaglesBagels@users.noreply.github.com>
2024-03-29 13:56:55 +1000

    Added test cases, need to create db

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -21,6 +21,11 @@ class TriviaTestCase(unittest.TestCase):
 
         self.client = self.app.test_client
 
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            self.db.create_all()
+
     
     def tearDown(self):
         """Executed after reach test"""
@@ -30,6 +35,66 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def populate_test_data(self):
+        """Populates the database with test data"""
+        # Insert categories
+        category1 = Category(type='Science')
+        category2 = Category(type='History')
+        self.db.session.add_all([category1, category2])
+        self.db.session.commit()
+
+        # Insert questions
+        question1 = Question(question='What is the capital of France?', answer='Paris', category=1, difficulty=2)
+        question2 = Question(question='Who invented the telephone?', answer='Alexander Graham Bell', category=1, difficulty=3)
+        self.db.session.add_all([question1, question2])
+        self.db.session.commit()
+
+    def test_get_categories(self):
+        """Test GET /categories"""
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'])
+        self.assertTrue(len(data['categories']) > 0)
+
+    def test_get_questions(self):
+        """Test GET /questions"""
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'])
+        self.assertTrue(len(data['questions']) > 0)
+        self.assertTrue(data['total_questions'] > 0)
+        self.assertTrue(len(data['categories']) > 0)
+
+    def test_404_get_questions_invalid_page(self):
+        """Test GET /questions with invalid page"""
+        res = self.client().get('/questions?page=1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertFalse(data['success'])
+
+    def test_delete_question(self):
+        """Test DELETE /questions/<question_id>"""
+        question_id = 1
+        res = self.client().delete(f'/questions/{question_id}')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'])
+        self.assertEqual(data['deleted'], question_id)
+
+    def test_404_delete_question_not_found(self):
+        """Test DELETE /questions/<question_id> with question not found"""
+        question_id = 1000
+        res = self.client().delete(f'/questions/{question_id}')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertFalse(data['success'])
 
 
 # Make the tests conveniently executable
